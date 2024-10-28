dbefef0bb66e81b9db6155a7bc0116394fa0def6
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2024-10-28 12:22:36 +0100

    [named tuples] implemented type diffing for named tuples #SCL-22569

--- scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/TypeDiff.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/TypeDiff.scala
@@ -5,7 +5,7 @@ import org.jetbrains.plugins.scala.annotator.Tree.{Leaf, Node}
 import org.jetbrains.plugins.scala.extensions._
 import org.jetbrains.plugins.scala.lang.psi.api.toplevel.typedef.ScClass
 import org.jetbrains.plugins.scala.lang.psi.types.api.presentation.TypePresentation
-import org.jetbrains.plugins.scala.lang.psi.types.api.{FunctionType, ParameterizedType, TupleType, Variance}
+import org.jetbrains.plugins.scala.lang.psi.types.api.{FunctionType, NamedTupleType, ParameterizedType, TupleType, Variance}
 import org.jetbrains.plugins.scala.lang.psi.types.{ScCompoundType, ScExistentialArgument, ScExistentialType, ScParameterizedType, ScType, TypePresentationContext}
 import org.jetbrains.plugins.scala.lang.refactoring.util.ScalaNamesUtil
 
@@ -107,6 +107,15 @@ object TypeDiff {
         if (ts1.length == ts2.length) Node(aMatch("("), Node((ts1 lazyZip ts2).map(diff).intersperse(aMatch(", ")): _*), aMatch(")"))
         else Node(aMismatch(tpe2.presentableText))
 
+      case (NamedTupleType(comps1), NamedTupleType(comps2)) =>
+        val diffComponent: ((ScType, ScType), (ScType, ScType)) => Tree[TypeDiff] = {
+          case ((name1, tpe1), (name2, tpe2)) =>
+            val name = NamedTupleType.NameType.from(name2).getOrElse(tpe2.presentableText)
+            Node(if (name1.conforms(name2)) aMatch(name) else aMismatch(name), aMatch(": "), diff(tpe1, tpe2)(conformance, context))
+        }
+
+        if (comps1.length == comps2.length) Node(aMatch("("), Node((comps1 lazyZip comps2).map(diffComponent).intersperse(aMatch(", ")): _*), aMatch(")"))
+        else Node(aMismatch(tpe2.presentableText))
       case (FunctionType(r1, p1), FunctionType(r2, p2)) =>
         val needsParens = (t: ScType) => FunctionType.isFunctionType(t) || TupleType.isTupleType(t)
         val left = {
