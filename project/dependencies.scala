798ca4bb1afec056e2172ae99b8cb3266d0dc16e
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2024-11-06 21:17:21 +0100

    Update intellijVersion to 251.4312

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -15,7 +15,7 @@ object Versions {
   val zincVersion = "1.10.4"
 
   // ATTENTION: check the comment in `Common.newProjectWithKotlin` when updating this version
-  val intellijVersion = "251.4141"
+  val intellijVersion = "251.4312"
 
   def isNightlyIntellijVersion: Boolean = intellijVersion.count(_ == '.') == 1
 
