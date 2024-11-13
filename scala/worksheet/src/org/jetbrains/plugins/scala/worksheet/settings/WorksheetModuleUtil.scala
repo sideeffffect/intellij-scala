cf21c990d6475e2ddd0def2e1899c20fd8a1a229
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2024-11-13 22:16:21 +0100

    [worksheet] Additionally, filter out modules which do not have a proper Scala standard library as a library dependency #SCL-23127

--- scala/worksheet/src/org/jetbrains/plugins/scala/worksheet/settings/WorksheetModuleUtil.scala
+++ scala/worksheet/src/org/jetbrains/plugins/scala/worksheet/settings/WorksheetModuleUtil.scala
@@ -2,14 +2,23 @@ package org.jetbrains.plugins.scala.worksheet.settings
 
 import com.intellij.openapi.module.Module
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.scala.project.ProjectExt
+import org.jetbrains.plugins.scala.project.{ModuleExt, ProjectExt}
 import org.jetbrains.sbt.SbtUtil
 
 private object WorksheetModuleUtil {
   def allProductionModulesWithScalaSdk(project: Project): Seq[Module] = {
     val separate = SbtUtil.isBuiltWithSeparateModulesForProdTest(project)
     val allScalaModules = project.modulesWithScala
+    val modulesWithScalaLibrary = allScalaModules.filter { module =>
+      val libraries = module.libraries
+      libraries.exists { library =>
+        val name = library.getName
+        name.contains("scala-library") || name.contains("scala3-library")
+      }
+    }
+    // In case there are no modules left, just use the non-filtered list. Some tests also start to fail in this case.
+    val modules = if (modulesWithScalaLibrary.nonEmpty) modulesWithScalaLibrary else allScalaModules
     // If the project has separate production and test modules enabled, only offer the production modules as a choice.
-    if (separate) allScalaModules.filter(_.getName.endsWith(".main")) else allScalaModules
+    if (separate) modules.filter(_.getName.endsWith(".main")) else modules
   }
 }
